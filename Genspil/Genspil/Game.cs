8fb0c7d5c656b117d1b9af0192d6ee5c90a758e5
Niklas Holm <niklasholm04@gmail.com>
2025-03-31 11:46:48 +0200

    Added game class

---
+++ Genspil/Genspil/Game.cs
@@ -0,0 +1,26 @@
+namespace Genspil;
+
+public class Game
+{
+    private int id;
+    private String name;
+    private String condition;
+    private double price;
+    private bool storageStatus;
+    private int amountInStorage;
+    private int playerNumber;
+    private String genre;
+
+    public Game(int id, string name, String condition, double price, bool storageStatus, int amountInStorage,
+        int playerNumber,  String genre)
+    {
+        this.id = id;
+        this.name = name;
+        this.condition = condition;
+        this.price = price;
+        this.storageStatus = storageStatus;
+        this.amountInStorage = amountInStorage;
+        this.playerNumber = playerNumber;
+        this.genre = genre;
+    }
+}
