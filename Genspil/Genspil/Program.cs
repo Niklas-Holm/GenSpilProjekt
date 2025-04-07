5874c8baabffa82c3b1fba9c36b2e1d4cf18dc8c
Gavin Denver <gavindenver@gmail.com>
2025-04-07 14:42:41 +0200

    Edited menus to reflect new method

--- Genspil/Genspil/Program.cs
+++ Genspil/Genspil/Program.cs
@@ -52,9 +52,11 @@ class Program
 
                     switch (listSubMenuInput)
                     {
+                        //Missing implementation for list generation
                         case 1:
                             Console.WriteLine("List Generated.");
                             break;
+                        //Sort list
                         case 2:
                             Console.WriteLine("How do you want to sort the list?");
                             Console.WriteLine("1. Name. \n" +
@@ -62,137 +64,27 @@ class Program
                                               "3. Price.\n" +
                                               "4. Min Player. \n" +
                                               "5. Max Player. \n" +
-                                              "6. Genre. \n"+
+                                              "6. Genre. \n" +
                                               "0. Exit.");
                             int filterGamesByInput = Int32.Parse(Console.ReadLine());
-                            switch (filterGamesByInput)
+
+                            Console.WriteLine("In What Order?\n" +
+                                                      "1. Ascending\n" +
+                                                      "2. Descending");
+                            int nameOrder = Int32.Parse(Console.ReadLine());
+
+                            storage.SortGamesBy(filterGamesByInput, nameOrder);
+
+                            Console.WriteLine("Generate text file for the sorted list? (y/n): ");
+                            string generateNameTxtInput = Console.ReadLine().ToLower();
+                            switch (generateNameTxtInput)
                             {
-                                case 1:
-                                    Console.WriteLine("In What Order?\n" +
-                                                      "1. Ascending\n" +
-                                                      "2. Descending");
-                                    int nameOrder = Int32.Parse(Console.ReadLine());
-                                    storage.FilterGameByName(nameOrder);
-                                    Console.WriteLine("Generate text file for the sorted list? (y/n): ");
-                                    string generateNameTxtInput = Console.ReadLine().ToLower();
-                                    switch (generateNameTxtInput)
-                                    {
-                                        case "y":
-                                            Console.WriteLine("Generating File");
-                                            break;
-                                        case "n":
-                                            
-                                            break;
-                                            
-                                    }
+                                case "y":
+                                    Console.WriteLine("Generating File");
                                     break;
-                                case 2:
-                                    Console.WriteLine("In What Order?\n" +
-                                                      "1. Ascending\n" +
-                                                      "2. Descending");
-                                    int conditionOrder = Int32.Parse(Console.ReadLine());
-                                    storage.FilterGameByCondition(conditionOrder);
-                                    Console.WriteLine("Generate text file for the sorted list? (y/n): ");
-                                    string generateConditionTxtInput = Console.ReadLine().ToLower();
-                                    switch (generateConditionTxtInput)
-                                    {
-                                        case "y":
-                                            Console.WriteLine("Generating File");
-                                            break;
-                                        case "n":
-
-                                            break;
-                                            
-                                    }
-
+                                case "n":
 
                                     break;
-
-                                case 3:
-                                    Console.WriteLine("In What Order?\n" +
-                                                      "1. Ascending\n" +
-                                                      "2. Descending");
-                                    int priceOrder = Int32.Parse(Console.ReadLine());
-                                    storage.FilterGameByPrice(priceOrder);
-                                    Console.WriteLine("Generate text file for the sorted list? (y/n): ");
-                                    string generatePriceTxtInput = Console.ReadLine().ToLower();
-                                    switch (generatePriceTxtInput)
-                                    {
-                                        case "y":
-                                            Console.WriteLine("Generating File");
-                                            break;
-                                        case "n":
-
-                                            break;
-                                            
-                                    }
-
-
-                                    break;
-                                case 4:
-                                    Console.WriteLine("In What Order?\n" +
-                                                      "1. Ascending\n" +
-                                                      "2. Descending");
-                                    int minPlayerOrder = Int32.Parse(Console.ReadLine());
-                                    storage.FilterGameByMinPlayer(minPlayerOrder);
-                                    Console.WriteLine("Generate text file for the sorted list? (y/n): ");
-                                    string generateMinPlayerTxtInput = Console.ReadLine().ToLower();
-                                    switch (generateMinPlayerTxtInput)
-                                    {
-                                        case "y":
-                                            Console.WriteLine("Generating File");
-                                            break;
-                                        case "n":
-
-                                            break;
-                                            
-                                    }
-
-
-                                    break;
-                                case 5:
-                                    Console.WriteLine("In What Order?\n" +
-                                                      "1. Ascending\n" +
-                                                      "2. Descending");
-                                    int maxPlayerOrder = Int32.Parse(Console.ReadLine());
-                                    storage.FilterGameByMinPlayer(maxPlayerOrder);
-                                    Console.WriteLine("Generate text file for the sorted list? (y/n): ");
-                                    string generateMaxPlayerTxtInput = Console.ReadLine().ToLower();
-                                    switch (generateMaxPlayerTxtInput)
-                                    {
-                                        case "y":
-                                            Console.WriteLine("Generating File");
-                                            break;
-                                        case "n":
-
-                                            break;
-                                            
-                                    }
-
-
-                                    break;
-                                case 6:
-                                    Console.WriteLine("In What Order?\n" +
-                                                      "1. Ascending\n" +
-                                                      "2. Descending");
-                                    int genreOrder = Int32.Parse(Console.ReadLine());
-                                    storage.FilterGameByGenre(genreOrder);
-                                    Console.WriteLine("Generate text file for the sorted list? (y/n): ");
-                                    string generateGenreTxtInput = Console.ReadLine().ToLower();
-                                    switch (generateGenreTxtInput)
-                                    {
-                                        case "y":
-                                            Console.WriteLine("Generating File");
-                                            break;
-                                        case "n":
-
-                                            break;
-                                            
-                                    }
-
-
-                                    break;
-
 
                             }
                             break;
